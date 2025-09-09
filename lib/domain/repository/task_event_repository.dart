10014848986876986a0ee3d5e58a6319bdd50871
Rodrigosdk <rodrigomamede53@gmail.com>
2025-09-08 23:43:58 -0300

    Refatora: Move TaskRules para a camada de aplicação e atualiza testes e implementações

--- lib/domain/repository/task_event_repository.dart
+++ lib/domain/repository/task_event_repository.dart
@@ -2,10 +2,10 @@ import '../entities/task_entity.dart';
 
 abstract class TaskEventRepository {
   List<TaskEntity> listen();
-  void save({required String title, String? description});
-  void updateStatus({required int id});
-  void updateTitle(int id, {required String name});
-  void updateDescription(int id, {required String description});
+  void save({required TaskEntity task});
+  void updateStatus({required TaskEntity task});
+  void updateTitle({required TaskEntity task});
+  void updateDescription({required TaskEntity task});
   void remove(int id);
 
 }
--- test/infrastructure/repositories/task_saving_test.dart
+++ test/infrastructure/repositories/task_saving_test.dart
@@ -1,61 +1,50 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todo/domain/entities/task_entity.dart';
 import 'package:todo/infrastructure/repositories/task_database_event.dart';
 
 
 void main() {
   late TaskDatabaseEvent repository;
 
-  late String title;
-  late String description;
+  late TaskEntity task;
 
   setUp((){
     repository = TaskDatabaseEvent();
 
-    title = "Estudar Flutter";
-    description = "Estudar Flutter na Udemy";
+    task = TaskEntity(
+      id: 1,
+      title: "Teste",
+      description: "Descrição",
+      completed: false,
+      createAt: DateTime.now(),
+      updateAt: DateTime.now(),
+    );
   });
 
   group("Tarefa", () {
 
-    test("deve conseguir ser salvar", () {
-      repository.save(title: title, description: description);
+    test("deve conseguir salvar", () {
+      repository.save(task: task);
 
       expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, description);
+      expect(repository.db[0].title, task.title);
+      expect(repository.db[0].description, task.description);
     });
 
-    test("deve conseguir salvar uma tarefa sem descrição", (){
-      repository.save(title: title);
-
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, null);
-    });
-
-
-    test("deve conseguir salvar como não concluída", () {
-      repository.save(title: title, description: description);
-
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, description);
-      expect(repository.db[0].completed, false);
-    });
 
     test("deve conseguir alterar o status de uma tarefa", () {
-      repository.save(title: title, description: description);
+      repository.save(task: task);
 
-      repository.updateStatus(id: 1);
+      repository.updateStatus(task: task.copyWith(completed: true));
 
       expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, description);
+      expect(repository.db[0].title, task.title);
+      expect(repository.db[0].description, task.description);
       expect(repository.db[0].completed, true);
     });
 
     test("deve conseguir ser removida", () {
-      repository.save(title: title, description: description);
+      repository.save(task: task);
 
       repository.remove(1);
 
@@ -63,138 +52,32 @@ void main() {
     });
 
     test("deve conseguir ser listada", () {
-      repository.save(title: title, description: description);
+      repository.save(task: task);
 
       final tasks = repository.listen();
 
       expect(repository.db.length, tasks.length);
     });
   });
+  test("deve conseguir alterar o título", () {
+    repository.save(task:task);
 
-  group("Campo título da tarefa", () {
-    test("deve conseguir alterar o título", () {
-      repository.save(title: title, description: description);
+    repository.updateTitle(task:task.copyWith(title: "Estudar Flutter"));
 
-      repository.updateTitle(1, name: "Estudar Flutter");
-
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, "Estudar Flutter");
-      expect(repository.db[0].description, description);
-      expect(repository.db[0].completed, false);
-    });
-
-    test("deve retornar um erro caso tente atualizar com um campo vazio", () {
-      repository.save(title: title, description: description);
-
-      expect(() => repository.updateTitle(1, name: ""), throwsException);
-      expect(() => repository.updateTitle(1, name: "  "), throwsException);
-    });
-
-
-    test("deve retornar um erro caso tente salvar uma tarefa com o título tendo mais de 20 caractere", () {
-      expect(
-            () => repository.save(title: "000000000000000000000000", description: description),
-        throwsException,
-      );
-    });
-
-    test("deve retornar um erro caso tente atualizar o título com mais de 20 caractere", () {
-      repository.save(title: title, description: description);
-
-      expect(
-        () => repository.updateTitle(1, name: "000000000000000000000000"),
-        throwsException,
-      );
-    });
-
-    test("deve modifcar o status para false caso altere o título", () {
-      repository.save(title: title, description: description);
-      repository.updateStatus(id: 1);
-
-      repository.updateTitle(1, name: "Estudar Flutter");
-
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, "Estudar Flutter");
-      expect(repository.db[0].description, description);
-      expect(repository.db[0].completed, false);
-    });
-
-    test("deve está alterando a data de atualização apos modificar o título",  () {
-        final beforeUpdate = DateTime.now();
-        repository.save(title: title, description: description);
-        repository.updateTitle(1, name: "Estudar Flutter");
-        final afterUpdate = DateTime.now();
-
-        expect(repository.db.length, 1);
-        expect(repository.db[0].title, "Estudar Flutter");
-        expect(repository.db[0].description, description);
-        expect(repository.db[0].completed, false);
-        expect(repository.db[0].updateAt!.isBefore(afterUpdate), true);
-        expect(repository.db[0].updateAt!.isAfter(beforeUpdate), true);
-      },
-    );
+    expect(repository.db.length, 1);
+    expect(repository.db[0].title, "Estudar Flutter");
+    expect(repository.db[0].description, task.description);
+    expect(repository.db[0].completed,  task.completed);
   });
 
-  group("Campo de descrição da tarefa", () {
-    test("deve conseguir alterar o descrição", () {
-      repository.save(title: title, description: description);
-      repository.updateDescription(1, description: "Estudar Flutter");
+  test("deve conseguir alterar o descrição", () {
+    repository.save(task:task);
 
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, "Estudar Flutter");
-      expect(repository.db[0].completed, false);
-    });
+    repository.updateTitle(task:task.copyWith(description: "Estudar Flutter"));
 
-    test("deve retornar um erro caso tente salvar com o valor vazio", () {
-      expect(
-            () => repository.save(title: title, description: ""),
-        throwsException,
-      );
-      expect(
-            () => repository.save(title: title, description: "    "),
-        throwsException,
-      );
-    });
-
-    test("deve retornar um erro caso o descrição seja vazio", () {
-      repository.save(title: title, description: description);
-
-      expect(
-        () => repository.updateDescription(1, description: ""),
-        throwsException,
-      );
-      expect(
-        () => repository.updateDescription(1, description: "  "),
-        throwsException,
-      );
-    });
-
-    test("deve modifcar o status para false caso altere o descrição", () {
-      repository.save(title: title, description: description);
-      repository.updateStatus(id: 1);
-
-      repository.updateDescription(1, description: "Estudar Flutter");
-
-      expect(repository.db.length, 1);
-      expect(repository.db[0].title, title);
-      expect(repository.db[0].description, "Estudar Flutter");
-      expect(repository.db[0].completed, false);
-    });
-
-    test("deve está alterando a data de atualização apos modificar o descrição", () {
-        final beforeUpdate = DateTime.now();
-        repository.save(title: title, description: description);
-        repository.updateDescription(1, description: "Estudar Flutter");
-        final afterUpdate = DateTime.now();
-
-        expect(repository.db.length, 1);
-        expect(repository.db[0].title, title);
-        expect(repository.db[0].description, "Estudar Flutter");
-        expect(repository.db[0].completed, false);
-        expect(repository.db[0].updateAt!.isBefore(afterUpdate), true);
-        expect(repository.db[0].updateAt!.isAfter(beforeUpdate), true);
-      },
-    );
+    expect(repository.db.length, 1);
+    expect(repository.db[0].title, task.title);
+    expect(repository.db[0].description, "Estudar Flutter");
+    expect(repository.db[0].completed, task.completed);
   });
 }
